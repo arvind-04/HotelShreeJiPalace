574f9f16a33404881978d9d48c7f2d00b40a9a90
Arvind Singh Chouhan <112876324+arvind-04@users.noreply.github.com>
2024-11-16 05:17:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Nav from './Components/Nav/Nav';
 import Header from './Components/Header/Header';
 import About from './Components/About/About';
